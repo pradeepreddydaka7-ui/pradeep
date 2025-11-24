728741d9c0d14b99b15b6accd4e81df530c85f98
pradeepreddydaka7-ui <pradeepreddydaka7@gmail.com>
2025-11-24 11:07:24 +0530

    Add files via upload

---
+++ SORT TECHNIQUE.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main() 
+{
+int n,i,j,temp,min;
+printf("enter size of array");
+scanf("%d",&n);
+int a[n];
+printf("enter array elements");
+ for(i=0;i<n;i++)
+ {
+scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+ 	min=i;
+ 	for(j=i+1;j<n;j++)
+ 	{
+ 		if(a[j]<a[min])
+ 		{
+ 			min=j;
+		 }
+	 }
+	 temp=a[i];
+	 a[i]=a[min];
+	 a[min]=temp;
+ }
+ for(i=0;i<n;i++)
+ printf("%d ",a[i]);
+}
