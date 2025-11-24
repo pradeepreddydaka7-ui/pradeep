f8836af2dc94e0cf45070816dd1865d70f5c88a9
pradeepreddydaka7-ui <pradeepreddydaka7@gmail.com>
2025-11-24 11:42:43 +0530

    Add files via upload

---
+++ initilisation string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+char str1[] = "Aditya";
+
+char str2[7] = "Aditya";
+
+char str3[7] = {'A','d','i','t','y','a','\0'};
+
+char str4[] = {'A','d','i','t','y','a','\0'};
+printf("%s\n",str1);  printf("%s\n",str2);  printf("%s\n",str3);  printf("%s\n",str4);
+return 0;
+}
